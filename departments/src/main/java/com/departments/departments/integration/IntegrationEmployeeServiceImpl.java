f12406252f69b66ee29d1e8ec64d43027c6f0deb
igormzhn <i.s.mezhenin@gmail.com>
2021-12-12 18:34:10 +0300

    forgiten file

--- departments/src/main/java/com/departments/departments/integration/IntegrationEmployeeServiceImpl.java
+++ departments/src/main/java/com/departments/departments/integration/IntegrationEmployeeServiceImpl.java
@@ -39,7 +39,7 @@ public class IntegrationEmployeeServiceImpl implements IntegrationEmployeeServic
 
     @Override
     public void moveEmployee(MoveEmployeeDto moveEmployee){
-        ResponseEntity<MoveEmployeeDto> res = employeeRestTemplate.postForEntity("http://192.168.1.6:8080/employees/move", moveEmployee, MoveEmployeeDto.class);
+        ResponseEntity<MoveEmployeeDto> res = employeeRestTemplate.postForEntity("http://localhost:8080/employees/move", moveEmployee, MoveEmployeeDto.class);
         if(!res.getStatusCode().equals(HttpStatus.OK)) {
             throw new IllegalArgumentException();       // сделать нормальное исключение
         }
